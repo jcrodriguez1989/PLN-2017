09b9ba82af2ab80c6d013867c366caa06362be28
jcrodriguez1989 <jcrodriguez@bdmg.com.ar>
2017-03-30 18:58:55 -0300

    Mi nuevo corpus funcionando

---
+++ languagemodeling/scripts/gettingMyCorpus.py
@@ -0,0 +1,60 @@
+from xml.dom import minidom
+import urllib.request
+#import urllib
+
+# usage: python gettingMyCorpus.py
+
+# en el corpus que baje, guarde 350 de los 9158 articulos de la query por que ya 
+# pesa 14M el archivo
+
+# downloads scientific papers from PubMedCentral, according to the desired query
+def PmcCorpusGenerator(query, outFile="myCorpus.txt"):
+    queryUrl = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"; # api de ncbi
+    database = "pmc"; # quiero articulos cientificos de PubMed Central
+    
+    completeUrl = queryUrl + "esearch.fcgi?db=" + database + "&term=" + query
+    completeUrl += "&retmax=40000"
+    xmlStr = urllib.request.urlopen(completeUrl).read()
+    #xmlStr = urllib.urlopen(completeUrl).read()
+    xmlDoc = minidom.parseString(xmlStr)
+    ids = xmlDoc.getElementsByTagName('Id')
+    i = 0
+    total = len(ids)
+    f = open(outFile, 'a') # append papers to our outFile
+    for actId in ids:
+        i += 1
+        actPaper = getPaper(actId.childNodes[0].data)
+        f.write(actPaper)
+        print(str(i) + " from " + str(total))
+    f.close()
+
+# gets all text from a nodelist object
+def getText(nodelist):
+    rc = []
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            rc.append(node.data)
+        else:
+            rc.append(getText(node.childNodes))
+    return ''.join(rc)
+
+def getPaper(actId):
+    downloadUrl = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+    downloadUrl = downloadUrl + "efetch.fcgi?db=pmc&id=" + actId
+    xmlStr = urllib.request.urlopen(downloadUrl).read()
+    #xmlStr = urllib.urlopen(downloadUrl).read()
+    xmlDoc = minidom.parseString(xmlStr)
+    
+    fullArticle = ""
+    
+    abstract = xmlDoc.getElementsByTagName('abstract')
+    if (len(abstract) > 0):
+        fullArticle += getText(abstract[0].childNodes)
+    
+    body = xmlDoc.getElementsByTagName('body')
+    if (len(body) > 0):
+        fullArticle += getText(body[0].childNodes)
+    return fullArticle
+
+
+PmcCorpusGenerator("\"breast+cancer\"AND\"gene+ontology\"")
--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -12,8 +12,9 @@ Options:
 from docopt import docopt
 import pickle
 
-from nltk.corpus import gutenberg
-# from nltk.corpus import PlaintextCorpusReader # If I want to use my own corpus
+#from nltk.corpus import gutenberg
+from nltk.corpus import PlaintextCorpusReader # If I want to use my own corpus
+from nltk.tokenize import RegexpTokenizer
 
 from languagemodeling.ngram import NGram
 
@@ -22,15 +23,21 @@ if __name__ == '__main__':
     opts = docopt(__doc__)
 
     # load the data
-    gutenbergAll = gutenberg.fileids()
-    gutenbergShake = [k for k in gutenbergAll if 'shakespeare' in k]
-    sents = gutenberg.sents(gutenbergShake)
-    # for excersice 1 as corpus we use gutenbergs Shakespeare works
-    
+    pattern = r'''(?ix)       # set flag to allow verbose regexps
+        (?:sr\.|sra\.)
+        | GO:[0-9]*           # gene ontology ids
+        | (?:[A-Z]\.)+        # abbreviations, e.g. U.S.A.
+        | \w+(?:-\w+)*        # words with optional internal hyphens
+        | \$?\d+(?:\.\d+)?%?  # currency and percentages, e.g. $12.40, 82%
+        | \.\.\.              # ellipsis
+        | [][.,;"'?():-_`]    # these are separate tokens; includes ], [
+        '''
+    tokenizer = RegexpTokenizer(pattern)
+
     # uncomment the following if I want to use my own corpus
-    #corpus = PlaintextCorpusReader('./languagemodeling/scripts/', 
-                                   #'myCorpus.txt')
-    #sents = corpus.sents()
+    corpus = PlaintextCorpusReader('./languagemodeling/scripts/', 
+                                   'myCorpus.txt', word_tokenizer=tokenizer)
+    sents = corpus.sents()
 
     # train the model
     n = int(opts['-n'])
