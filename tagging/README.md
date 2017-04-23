cb00c9631d2ddcefb43c503f23900874923063b9
jcrodriguez1989 <jcrodriguez@bdmg.com.ar>
2017-04-22 21:18:54 -0300

    Ejercicio 1 terminado.

---
+++ tagging/README.md
@@ -0,0 +1,44 @@
+# PLN 2017: Procesamiento de Lenguaje Natural 2017
+## Práctico 2
+### Ejercicio 1
+
+Se programó el script stats.py que obtiene estadísticas básicas del corpus Ancora.
+Nota: Estas estadísticas no están diferenciando entre mayusculas y minúsculas.
+
+Cantidad de oraciones: 17,379
+Cantidad de ocurrencias de palabras: 517,269
+Cantidad de palabras (vocabulario): 46,482
+Cantidad de etiquetas (vocabulario de tags): 48
+
+Etiquetas más frecuentes:
+
+| tag  | Frecuencia | Porcentaje | Palabras más frecuentes | Descripción |
+| ---- |:---------- |:---------- |:----------------------- |:----------- |
+| 'nc' | 92,003     | 0.1778     | 'años', 'presidente', 'millones', 'equipo', 'partido' | Sustantivo |
+| 'sp' | 79,904     | 0.1544     | 'de', 'en', 'a', 'del', 'con' | Preposición |
+| 'da' | 54,552     | 0.1054     | 'la', 'el', 'los', 'las', 'El' | Artículo |
+| 'vm' | 50,609     | 0.0978     | 'está', 'tiene', 'dijo', 'puede', 'hace' | Verbo |
+| 'aq' | 33,904     | 0.0655     | 'pasado', 'gran', 'mayor', 'nuevo', 'próximo' | Adjetivo |
+| 'fc' | 30,148     | 0.0582     | ',' | Coma |
+| 'np' | 29,113     | 0.0562     | 'Gobierno', 'España', 'PP', 'Barcelona', 'Madrid' | Pronombre personal |
+| 'fp' | 21,157     | 0.0409     | '.', '(', ')' | Punto / paréntesis |
+| 'rg' | 15,333     | 0.0296     | 'más', 'hoy', 'también', 'ayer', 'ya' | Adverbio |
+| 'cc' | 15,023     | 0.0290     | 'y', 'pero', 'o', 'Pero', 'e' | Conjunción |
+
+
+Niveles de ambigüedad de las palabras:
+
+| Nivel | #Palabras | Porcentaje | Palabras más frecuentes |
+| ---- |:---------- |:---------- |:----------------------- |
+| 1    | 44,109     | 0.0852     | ',', 'el', 'en', 'con', 'por' |
+| 2    |  2,194     | 0.0042     | 'la', 'y', '"', 'los', 'del' |
+| 3    |    153     | 0.0002     | '.', 'a', 'un', 'no', 'es' |
+| 4    |     19     |      0     | 'de', 'dos', 'este', 'tres', 'todo' |
+| 5    |      4     |      0     | 'que', 'mismo', 'cinco', 'medio' |
+| 6    |      3     |      0     | 'una', 'como', 'uno' |
+| 7    |      0     |      0     | - |
+| 8    |      0     |      0     | - |
+| 9    |      0     |      0     | - |
+
+### Ejercicio 2
+
--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -23,11 +23,51 @@ if __name__ == '__main__':
     # compute the statistics
     print('sents: {}'.format(len(sents))) # number of sentences
 
-    counts = defaultdict(int)
+    word_counts = defaultdict(int)
+    tag_counts = defaultdict(int)
+    word_tags = defaultdict(lambda : defaultdict(int))
+    tag_words = defaultdict(lambda : defaultdict(int))
     for sent in sents:
         for key,val in sent:
-            counts[key] += 1
+            val = val[:2]
+            word_counts[key] += 1
+            tag_counts[val] += 1
+            word_tags[key][val] += 1
+            tag_words[val][key] += 1
 
-    print('tokens: {}'.format(len(counts))) # number of tokens
-    print('words: {}'.format(sum([ counts[key] for key in counts.keys() ])))
+    print('tokens: {}'.format(len(word_counts))) # number of tokens
+    print('words: {}'.format(sum([ word_counts[key] for key in word_counts.keys() ])))
     # number of words
+    print('tags: {}'.format(len(tag_counts))) # number of tags
+
+    tag_counts_list = list(tag_counts.items())
+    tag_counts_list.sort(key=lambda x: x[1], reverse=not False)
+    print('most frequent tags')
+    most_freq_tags = tag_counts_list[:10]
+    print([ val[0] for val in most_freq_tags ])
+    print([ val[1] for val in most_freq_tags ])
+    total_tags = sum([val[1] for val in tag_counts_list])
+    print([ val[1]/total_tags for val in most_freq_tags ])
+
+    for tag,_ in most_freq_tags:
+        act_tag_words = list(tag_words[tag].items())
+        act_tag_words.sort(key=lambda x: x[1], reverse=not False)
+        print("\n", tag, ":")
+        print([ val[0] for val in act_tag_words][:5])
+
+    # ambiguety
+    ambiguety = [list() for _ in range(9)]
+    for word_tag in word_tags.items():
+        if (len(word_tag[1]) == 0):
+            continue
+        act_level = ambiguety[len(word_tag[1])-1]
+        act_level.append(word_tag)
+
+    for i in range(9):
+        print("ambiguety", i+1)
+        act_level = ambiguety[i]
+        print(len(act_level))
+        print(len(act_level) / total_tags)
+        act_level.sort(key=lambda x: sum(x[1].values()), reverse=not False)
+        print([ val[0] for val in act_level][:5])
+
