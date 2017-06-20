b55f1a2e8bfa8557159e840c973e37fa6455d306
jcrodriguez1989 <jcrodriguez@bdmg.com.ar>
2017-06-20 10:09:43 -0300

    Avanzando un poco

--- relatedpapers/scripts/example.R
+++ relatedpapers/scripts/example.R
@@ -1,29 +1,31 @@
 setwd('/media/jcrodriguez/Data11/Git/PLN-2017/relatedpapers/scripts/');
 
-gsetsInfo <- read.csv('gsetsInfo.csv', sep='\t');
+rm(list=ls());
+gsetsInfo <- read.csv('gsetsInfo2.csv', sep='\t');
 dim(gsetsInfo);
-# [1] 492  10
+# [1] 492  30
 
-## analicemos basal
-
+require(GO.db);
 getQueries <- function(data, stype, onlyLeaf=!F, minDepth=0) {
+#     data <- gsetsInfo; stype <- "Ba"; onlyLeaf=!F; minDepth=0;
     stopifnot(all(c("ID", "Depth", "IsLeaf") %in% colnames(data)));
     stopifnot(stype %in% colnames(data));
     actData <- data;
     if (onlyLeaf) {
-        actData <- actData[actData$IsLeaf,];
+        actData <- actData[as.logical(actData$IsLeaf),];
     }
     
-    actData <- actData[actData[,stype] != "",];
+    actData <- actData[actData[,stype] == 1,];
+    print(paste(stype, nrow(actData)));
+    stopifnot(is(actData$Depth, 'integer'));
     actData <- actData[actData$Depth >= minDepth,];
     
-    require(GO.db);
     queries <- do.call(c, lapply(as.character(actData$ID), function(gset) {
-        gset2 <- gsub('GO:' , '', gset);
-        gset3 <- gsub('^0*' , '', gset2);
+#         gset2 <- gsub('GO:' , '', gset);
+#         gset3 <- gsub('^0*' , '', gset2);
         term <- Term(gset);
         
-        query <- paste(gset, gset2, gset3, term, sep='"OR"');
+        query <- paste(gset, term, sep='"OR"');
         
         synonyms <- unlist(Synonym(gset));
         if (!is.null(synonyms)) {
@@ -36,6 +38,17 @@ getQueries <- function(data, stype, onlyLeaf=!F, minDepth=0) {
     return(data.frame(ID=actData$ID, queries=queries))
 }
 
+subtypes <- c('Ba', 'B', 'A', 'H');
+invisible(lapply(subtypes, function(actStype) {
+    queries <- getQueries(gsetsInfo, actStype, onlyLeaf=F);
+#     print(nrow(queries));
+    if (nrow(queries) > 0) {
+        write.table(queries, file=paste(actStype, '.csv', sep=''), sep="\t", row.names=F, quote=F, col.names=F);
+    }
+}))
+
+
+## analicemos basal
 queriesBa <- getQueries(gsetsInfo, "Ba");
 queriesBa[7,]
 # "(\"GO:0071073\"OR\"0071073\"OR\"71073\"OR\"positive regulation of phospholipid biosynthetic process\"OR\"activation of phospholipid biosynthetic process\"OR\"positive regulation of phospholipid anabolism\"OR\"positive regulation of phospholipid biosynthesis\"OR\"positive regulation of phospholipid formation\"OR\"positive regulation of phospholipid synthesis\"OR\"stimulation of phospholipid biosynthetic process\"OR\"up regulation of phospholipid biosynthetic process\"OR\"up-regulation of phospholipid biosynthetic process\"OR\"upregulation of phospholipid biosynthetic process\")"
